c352a0e5ee38e1a5a9e21f7c4f9014f79483c687
Alain Claude <63712848+ALAIN-CLAUDE@users.noreply.github.com>
2024-08-23 23:46:32 +0100

    Update

--- force-app/main/default/lwc/sitePortalLeftBanner/sitePortalLeftBanner.js
+++ force-app/main/default/lwc/sitePortalLeftBanner/sitePortalLeftBanner.js
@@ -1,9 +1,8 @@
 import { LightningElement, track } from 'lwc';
-//import hydeLoginCaption from '@salesforce/label/c.hydeLoginCaption5';
 import FORM_FACTOR from '@salesforce/client/formFactor';
 
 export default class SitePortalLeftBanner extends LightningElement {  
-    //loginText = hydeLoginCaption;
+
     @track isDesktopOrTablet;
   
     connectedCallback() {
